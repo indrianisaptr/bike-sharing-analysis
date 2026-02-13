97bfa1eac00749569ced4e395ea6e723541d6748
Kadek Indri Anisa Putri <indrianisaputri14@gmail.com>
2026-02-13 15:30:12 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Cara menjalankan Dashboard:
+pip install -r requirements.txt
+streamlit run dashboard/dashboard.py
---
+++ dashboard/dashboard.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.title("Bike Sharing Analysis Dashboard")
+
+df = pd.read_csv("Submission Proyek Analisis Data/dashboard/main_data.csv")
+
+menu = st.selectbox("Pilih Analisis", 
+                    ["Musim", "Cuaca", "Hari Kerja"])
+
+if menu == "Musim":
+    data = df.groupby("season")["cnt"].mean().reset_index()
+    fig, ax = plt.subplots()
+    sns.barplot(data=data, x="season", y="cnt", ax=ax)
+    st.pyplot(fig)
+
+elif menu == "Cuaca":
+    data = df.groupby("weathersit")["cnt"].mean().reset_index()
+    fig, ax = plt.subplots()
+    sns.barplot(data=data, x="weathersit", y="cnt", ax=ax)
+    st.pyplot(fig)
+
+else:
+    data = df.groupby("workingday")["cnt"].mean().reset_index()
+    fig, ax = plt.subplots()
+    sns.barplot(data=data, x="workingday", y="cnt", ax=ax)
+    st.pyplot(fig)
